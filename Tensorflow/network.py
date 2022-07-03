d30196ef70cd3dbcf3941fe6ef3b1273d1b5937e
MGaloto <maxigaloto@hotmail.com>
2022-07-03 18:40:44 -0300

    nn plot

--- Tensorflow/network.py
+++ Tensorflow/network.py
@@ -1,7 +1,7 @@
 from termcolor import colored
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt
 
 class Model():
     def __init__(self):
@@ -17,7 +17,6 @@ class Model():
                 print('Muchas gracias\n')
                 break
             elif modelo == 'Si':
-                print('Excelente...')
                 scaler   = input('Desea escalar los valores? (Si/No)').lower()
                 neuronas = int(input(f'Ingrese la cantidad de Neuronas:'))
                 outputs  = int(input(f'Ingrese la cantidad de Outputs:'))
@@ -68,6 +67,8 @@ class Model():
         print(self._colored(233, 128 ,13,f'\n\nPerdida:  {perdida}'))
         print(self._colored(233, 75, 13,f'Accuracy: {acc}\n\n'))
         print(self._colored(233, 75, 13,'OK Model\n'))
+        print(self._colored(233, 75, 13, model.evaluate(X_test, y_test)))
+
         return historial
 
     def _get_plots(self, metricas, hyper_p, y_label = 'Metrics'):
@@ -88,3 +89,28 @@ class Model():
         plt.xlabel('Epocas', fontsize=16); plt.ylabel(y_label, fontsize=16)
         return plt.grid(); plt.show()
         
+
+
+    def _draw_neural_net(self, ax, left, right, bottom, top, layer_sizes):
+        n_layers = len(layer_sizes)
+        v_spacing = (top - bottom)/float(max(layer_sizes))
+        h_spacing = (right - left)/float(len(layer_sizes) - 1)
+        # Nodes
+        for n, layer_size in enumerate(layer_sizes):
+            layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
+            for m in range(0 , layer_size + 1):
+                circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4.,
+                                    color='w', ec='k', zorder=3)
+                ax.legend([circle], ['Inputs - Hidden Labels - Outputs'], loc='upper left')
+                ax.add_artist(circle)
+        # Edges
+        for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+            layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
+            layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
+            
+            for m in range(0 ,layer_size_a +1):
+
+                for o in range(0, layer_size_b + 1):
+                    line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
+                                    [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='b')
+                    ax.add_artist(line)
